31f084a56475a0cd1681a88548d19db463de3d3d
Denny Setyawan Huda <dennshgit@gmail.com>
2023-02-21 21:52:48 +0700

    feat: extend color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        first: "#FFD800",
+        second: "#262626",
+      },
+    },
   },
   plugins: [],
 };
