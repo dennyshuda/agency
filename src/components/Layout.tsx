783d41724f74413b917bb8ccc30677231eec89b4
Denny Setyawan Huda <dennshgit@gmail.com>
2023-02-25 12:58:51 +0700

    feat: create layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+interface Children {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: Children) {
+  return <div className="container mx-auto px-10">{children}</div>;
+}
