8860f9c22e951a67b19f0b97156ef2050f1d6393
Denny Setyawan Huda <dennshgit@gmail.com>
2023-02-25 12:57:43 +0700

    chore: adjust screen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
     extend: {
       colors: {
         first: "#FFD800",
